38fcbafe6782f2d4a07067489e64f225ec70998e
mbiopeter <110722910+mbiopeter@users.noreply.github.com>
2024-09-11 14:37:57 +0300

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,6 @@ import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { logo } from "../../assets/index"
 import { navLinksdata } from '../../constants';
 
 const Navbar = () => {
